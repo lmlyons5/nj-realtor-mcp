fbfbda72cdfd922a50738a557574fac071024135
lu <lmlyons5@gmail.com>
2026-06-27 22:29:40 -0400

    add missing pdf_report stub

---
+++ src/utils/pdf_report.js
@@ -0,0 +1,9 @@
+/**
+ * pdf_report.js
+ * Stub for PDF report generation. Returns null until a PDF library is integrated.
+ */
+
+export async function generatePDFReport(type, data) {
+  // TODO: Integrate a PDF library (e.g. PDFKit, Puppeteer) to generate real reports
+  return null;
+}
